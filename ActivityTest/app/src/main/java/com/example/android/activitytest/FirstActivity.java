9201aa6c46dd902286368935d7b9685747cc6b32
caiwubin <84606895@qq.com>
2018-10-11 23:49:04 +0800

    basic usage of Activity

---
+++ ActivityTest/app/src/main/java/com/example/android/activitytest/FirstActivity.java
@@ -0,0 +1,86 @@
+package com.example.android.activitytest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    /**
+     * Dispatch incoming result to the correct fragment.
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if(resultCode==RESULT_OK) {
+                    String returnData=data.getStringExtra("data_return");
+                    Log.d("FirstActivity",returnData);
+                }
+            default:
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1=findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Toast.makeText(FirstActivity.this,"You clicked Button 1",Toast.LENGTH_SHORT).show();
+               // finish();
+                //显示启动
+                //Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                //startActivity(intent);
+                //隐式启动
+               // Intent intent = new Intent("com.example.android.activitytest.ACTION_START");
+               // intent.addCategory("com.example.android.activitytest.MY_CATEGORY");
+               // Intent intent =new Intent(Intent.ACTION_VIEW);
+              //  intent.setData(Uri.parse("http://www.baidu.com"));
+                //startActivity(intent);
+//                String data="Hello SecondActivity";
+//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                intent.putExtra("extra_data",data);
+//                startActivity(intent);
+                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+
+    }
+
+    //重写方法，第一个参数用于指定我们通过哪一个资源文件来创建菜单，第二个参数用于指定我们的菜单项添加到哪一个Menu对象当中
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+}
