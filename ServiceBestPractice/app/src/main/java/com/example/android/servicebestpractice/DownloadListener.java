1c8f1fcf8e2212718e4372cf550a77bad541c0ed
caiwubin <84606895@qq.com>
2018-12-03 21:46:22 +0800

    Service Practice

---
+++ ServiceBestPractice/app/src/main/java/com/example/android/servicebestpractice/DownloadListener.java
@@ -0,0 +1,19 @@
+package com.example.android.servicebestpractice;
+
+public interface DownloadListener {
+
+    //通知当前的下载进度
+    void onProgress(int progress);
+
+    //通知下载成功事件
+    void onSuccess();
+
+    //通知下载失败事件
+    void onFailed();
+
+    //通知下载暂停事件
+    void onPaused();
+
+    //通知下载取消事件
+    void onCanceled();
+}
